203442805682d20b219763ed07e26be33f50ae1c
Keven <kmax136@gmail.com>
2019-12-01 13:41:55 -0300

    add jobs

---
+++ src/app/jobs/RegistrationMail.js
@@ -0,0 +1,15 @@
+import Mail from "../lib/Mail";
+
+export default {
+  key: "RegistrationMail",
+  async handle({ data }) {
+    const { user } = data;
+
+    await Mail.sendMail({
+      from: "Queue Test <kmax136@gmail.com>",
+      to: `${user.name} <${user.email}>`,
+      subject: "Cadastro de Usuário",
+      html: `Olá ${user.name}, bem-vindo ao sistema de filas`
+    });
+  }
+};
