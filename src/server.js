24c9c3e6bedbf00d097d71094269345f97696e65
Keven <kmax136@gmail.com>
2019-12-01 13:45:12 -0300

    adding config to bull board

--- src/server.js
+++ src/server.js
@@ -1,11 +1,17 @@
 import "dotenv/config";
 import express from "express";
 import routes from "./routes";
+import BullBoard from "bull-board";
+import Queue from "./app/lib/Queue";
 
 const app = express();
 
+BullBoard.setQueues(Queue.queues.map(queue => queue.bull));
+
 app.use(express.json());
 app.use(routes);
+
+app.use("/admin/queues", BullBoard.UI);
 
 app.listen(3000, () => {
   console.log("Server running on localhost:3000");
