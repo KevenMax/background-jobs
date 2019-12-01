6e6eaf263b6ff3ec42d147dcb7b504496a2a64a4
Keven <kmax136@gmail.com>
2019-12-01 02:46:27 -0300

    creating route and controller to User

---
+++ src/app/controllers/UserController.js
@@ -0,0 +1,12 @@
+export default {
+  async store(req, res) {
+    const { name, email, password } = req.body;
+
+    const user = {
+      name,
+      email,
+      password
+    };
+    return res.json(user);
+  }
+};
