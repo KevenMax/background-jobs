5e3f631c0e99963be5493f05b28aac44692ea7a0
Keven <kmax136@gmail.com>
2019-12-01 11:43:41 -0300

    add nodemailer

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,3 +1,5 @@
+import Mail from "../lib/Mail";
+
 export default {
   async store(req, res) {
     const { name, email, password } = req.body;
@@ -7,6 +9,13 @@ export default {
       email,
       password
     };
+    await Mail.sendMail({
+      from: "Queue Test <kmax136@gmail.com>",
+      to: `${name} <${email}>`,
+      subject: "Cadastro de Usuário",
+      html: `Olá ${name}, bem-vindo ao sistema de filas`
+    });
+
     return res.json(user);
   }
 };
